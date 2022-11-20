2f23a35cdce3c48c2b28130d8a6a7cf978438c4c
shawntez32 <shawntez.malone1@gmail.com>
2022-11-19 19:46:35 -0500

    deploying app to gh pages

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a>Home</a>
+      <a>Products</a>
+      <a>Join Us</a>
+      <a>Contact Us</a>
     </div>
   );
 }
