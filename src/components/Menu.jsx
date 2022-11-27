a40867dcea464a0fa702cb93fa7a8f8933405db7
shawntez32 <shawntez.malone1@gmail.com>
2022-11-27 11:13:11 -0500

    update

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -14,7 +14,11 @@ function Menu() {
             <div>
                 <h2>Dinners</h2>
              <div className="foodBox">
+                <div>
                 <img src={burger} alt="Burger"></img>
+                <h4 className="image-text">Build-It Burger</h4>
+                </div>
+                
                     <div className="foodDsc">
                     <h3>Burgers</h3>
                     <p>All beef patty served with your choice of tasty toppings.</p>
@@ -57,6 +61,7 @@ function Menu() {
                <div className="foodBox">
                <img src={romaine} alt="Romaine Lettuce"></img>
                <div className="foodDsc">
+                <h3>Romaine Lettuce</h3>
                 <p>Everyday fresh romaine Lettuce</p>
                 <a>Order Now</a>
                </div>
