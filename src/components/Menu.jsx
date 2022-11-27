e08fb354135fe80615d290ab3af2f4af811d00cb
shawntez32 <shawntez.malone1@gmail.com>
2022-11-27 11:08:14 -0500

    update

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -15,7 +15,7 @@ function Menu() {
                 <h2>Dinners</h2>
              <div className="foodBox">
                 <img src={burger} alt="Burger"></img>
-                    <div>
+                    <div className="foodDsc">
                     <h3>Burgers</h3>
                     <p>All beef patty served with your choice of tasty toppings.</p>
                     <a>Order Now</a>
@@ -23,8 +23,12 @@ function Menu() {
              </div>
                 
             <div className="foodBox">
-                <img src={cheese} alt="Fries"></img>
                 <div>
+                <img src={cheese} alt="Fries"></img>
+                <h4 className="img-text">Famous fries</h4>
+                </div>
+                
+                <div className="foodDsc">
                 <h3>Fries</h3>
                 <p>Our famous fries.</p>
                 <a>Order Now</a>
@@ -41,7 +45,7 @@ function Menu() {
         <div>
                <div className="foodBox">
                <img src={iceberg} alt="Iceberg Lettuce"></img>
-               <div>
+               <div className="foodDsc">
                 <h3>Iceberg Salad</h3>
                 <p>Create your own everyday fresh iceberg Lettuce</p>
                 <a>Order Now</a>
@@ -52,7 +56,7 @@ function Menu() {
             <div>
                <div className="foodBox">
                <img src={romaine} alt="Romaine Lettuce"></img>
-               <div>
+               <div className="foodDsc">
                 <p>Everyday fresh romaine Lettuce</p>
                 <a>Order Now</a>
                </div>
@@ -66,14 +70,14 @@ function Menu() {
             <h2>Soup</h2>
             <div className="foodBox">
                <img src={soup} alt="Soup"></img>
-               <div>
+               <div className="foodDsc">
                 <p>Delicious make your own soup</p>
                 <a>Order Now</a>
             </div>
             </div>
             <div className="foodBox">
                <img src={soup2} alt="Soup"></img>
-               <div>
+               <div className="foodDsc">
                 <p>Delicious make your own soup</p>
                 <a>Order Now</a>
             </div>
